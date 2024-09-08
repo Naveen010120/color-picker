1b3d6631b2aa5426016eacd1b34684968738d2aa
Naveen010120 <raghuvamshinaveensingh@gmail.com>
2024-09-08 23:42:45 +0530

    color picker

---
+++ javaScript.js
@@ -0,0 +1,27 @@
+let body=document.getElementById("total-content");
+let textColor=document.getElementById("colorChange")
+function button1(){
+    body.style.backgroundColor="#e0e0e0";
+    textColor.innerHTML="#e0e0e0";
+    textColor.style.color="#e0e0e0";
+}
+function button2(){
+    body.style.backgroundColor="#6fcf97";
+    textColor.innerHTML="#6fcf97";
+    textColor.style.color="#6fcf97";
+
+
+}
+function button3(){
+    body.style.backgroundColor="#56ccf2";
+    textColor.innerHTML="#56ccf2";
+    textColor.style.color="#56ccf2";
+
+
+}
+function button4(){
+    body.style.backgroundColor="#bb6bd9";
+    textColor.innerHTML="#bb6bd9";
+    textColor.style.color="#bb6bd9";
+    
+}
